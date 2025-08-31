763f22d6303d281b331b60f78950c07c5843d5f6
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-09-01 00:01:54 +0200

    initial commit: Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Book
 
 # Register your models here.
+@admin.register(Book)
+class BooKAdmin(admin.ModelAdmin):
+   list_display = ("title", "author", "publication_date")
+   list_filter = ("author", "publication_date")
+   search_fields = ("title", "author")
+   date_hierarchy = "publication_date"
