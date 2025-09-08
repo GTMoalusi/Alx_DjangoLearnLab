667ef5bc6b2b5aa801204d12f0974651c580240c
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-09-08 21:20:57 +0200

    initial commit: Implementing Advanced Model Relationships in Django

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Maps the URL "books/" to the list_books view
+    path('books/', views.list_books, name='list_books'),
+    
+    # Maps a URL like "library/1/" to the LibraryDetailView
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
