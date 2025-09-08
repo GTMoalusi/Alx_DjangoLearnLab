c7970711a182c10c0029958c4cfb181eb53d23ba
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-09-08 21:54:37 +0200

    initial commit: Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import Book, Library
 from django.views.generic import DetailView
+from .models import Book
+from .models import Library
+from django.views.generic.detail import DetailView
 
+# Function-based view to list all books
 def list_books(request):
     """
     Retrieves all Book objects from the database and renders a list.
@@ -12,6 +15,7 @@ def list_books(request):
     }
     return render(request, 'relationship_app/list_books.html', context)
 
+# Class-based view to display details for a specific library
 class LibraryDetailView(DetailView):
     """
     Displays the details of a single Library object.
