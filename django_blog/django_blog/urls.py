32504015ff87cb1b44fa38ff68dcfea3777de955
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-10-12 21:30:36 +0200

    initial commit: Initial Setup and Project Configuration for a Django Blog

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -18,18 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Admin site URL
+    # Admin path remains the same
     path('admin/', admin.site.urls),
-
-    # Include the blog application's URLs at the root of the project.
-    # This is necessary for Django to find the 'home' and 'posts' names.
-    path('', include('blog.urls')),
-
-    # This makes 'login' and 'logout' (and others) available under the 'accounts/' path.
-    path('accounts/', include('django.contrib.auth.urls')), 
-
-]
-
-
     
-
+    # NEW: Include the blog application's URLs.
+    # We map the root path ('') to the blog app and specify a namespace 'blog'.
+    # This allows template tags like {% url 'blog:home' %} to work.
+    path('', include('blog.urls')),
+    
+    # You might also need this line if you are handling authentication (login/logout) 
+    # outside of the blog app, which is a common practice.
+    path('accounts/', include('django.contrib.auth.urls')),
+]
